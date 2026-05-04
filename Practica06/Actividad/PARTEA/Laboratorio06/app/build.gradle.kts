8dac2f6bd34097aec55b10e422e491f4b8bf720d
HectorHinojosa29 <hinojosahector29@gmail.com>
2026-05-04 15:26:02 -0500

    Commit

---
+++ Practica06/Actividad/PARTEA/Laboratorio06/app/build.gradle.kts
@@ -0,0 +1,58 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.compose)
+}
+
+android {
+    namespace = "com.ucsm.laboratorio06"
+    compileSdk {
+        version = release(36) {
+            minorApiLevel = 1
+        }
+    }
+
+    defaultConfig {
+        applicationId = "com.ucsm.laboratorio06"
+        minSdk = 24
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    buildFeatures {
+        compose = true
+    }
+}
+
+dependencies {
+    // Material 3 Components (View system clásico)
+    implementation("com.google.android.material:material:1.12.0")
+    // AndroidX base
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.0")
+    implementation("androidx.activity:activity-ktx:1.9.3")
+    // Jetpack Compose (recomendado para apps nuevas)
+    val composeBom = platform("androidx.compose:compose-bom:2026.03.00")
+    implementation(composeBom)
+    implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    debugImplementation("androidx.compose.ui:ui-tooling")
+    implementation("androidx.activity:activity-compose:1.9.3")
+
+    implementation("androidx.compose.material:material-icons-extended")
+}
---
+++ Practica06/Actividad/PARTEC/flutter_lab06/android/app/src/main/java/com/ucsm/flutter_lab06/MainActivity.java
@@ -0,0 +1,6 @@
+package com.ucsm.flutter_lab06;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
